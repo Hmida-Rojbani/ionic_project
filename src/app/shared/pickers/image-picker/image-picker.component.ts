a02b9c39272ff93e3ad202a864ee34bd501941ce
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-02 17:48:21 +0100

    add pwa camera

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 import {
   Plugins,
   Capacitor,
   CameraSource,
   CameraResultType
 } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -12,15 +20,30 @@ import {
   styleUrls: ['./image-picker.component.scss']
 })
 export class ImagePickerComponent implements OnInit {
-  @Output() imagePick = new EventEmitter<string>();
+  @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<HTMLInputElement>;
+  @Output() imagePick = new EventEmitter<string | File>();
   selectedImage: string;
+  usePicker = false;
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('Mobile:', this.platform.is('mobile'));
+    console.log('Hybrid:', this.platform.is('hybrid'));
+    console.log('iOS:', this.platform.is('ios'));
+    console.log('Android:', this.platform.is('android'));
+    console.log('Desktop:', this.platform.is('desktop'));
+    if (
+      (this.platform.is('mobile') && !this.platform.is('hybrid')) ||
+      this.platform.is('desktop')
+    ) {
+      this.usePicker = true;
+    }
+  }
 
   onPickImage() {
     if (!Capacitor.isPluginAvailable('Camera')) {
+      this.filePickerRef.nativeElement.click();
       return;
     }
     Plugins.Camera.getPhoto({
@@ -37,7 +60,24 @@ export class ImagePickerComponent implements OnInit {
       })
       .catch(error => {
         console.log(error);
+        if (this.usePicker) {
+          this.filePickerRef.nativeElement.click();
+        }
         return false;
       });
+  }
+
+  onFileChosen(event: Event) {
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if (!pickedFile) {
+      return;
+    }
+    const fr = new FileReader();
+    fr.onload = () => {
+      const dataUrl = fr.result.toString();
+      this.selectedImage = dataUrl;
+      this.imagePick.emit(pickedFile);
+    };
+    fr.readAsDataURL(pickedFile);
   }
 }
