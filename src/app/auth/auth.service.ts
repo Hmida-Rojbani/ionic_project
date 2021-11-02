6056b8b8058e687353a0399e122c87675eb6ddb9
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-02 17:55:46 +0100

    auth 3 stat management

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
 import { environment } from '../../environments/environment';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -16,38 +20,71 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
-  private _userId = null;
+  private _user = new BehaviorSubject<User>(null);
 
   get userIsAuthenticated() {
-    return this._userIsAuthenticated;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return !!user.token;
+        } else {
+          return false;
+        }
+      })
+    );
   }
 
   get userId() {
-    return this._userId;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return user.id;
+        } else {
+          return null;
+        }
+      })
+    );
   }
 
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
-        environment.firebaseAPIKey
-      }`,
-      { email: email, password: password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
+          environment.firebaseAPIKey
+        }`,
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(tap(this.setUserData.bind(this)));
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
-        environment.firebaseAPIKey
-      }`,
-      { email: email, password: password }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
+          environment.firebaseAPIKey
+        }`,
+        { email: email, password: password }
+      )
+      .pipe(tap(this.setUserData.bind(this)));
   }
 
   logout() {
-    this._userIsAuthenticated = false;
+    this._user.next(null);
+  }
+
+  private setUserData(userData: AuthResponseData) {
+    const expirationTime = new Date(
+      new Date().getTime() + +userData.expiresIn * 1000
+    );
+    this._user.next(
+      new User(
+        userData.localId,
+        userData.email,
+        userData.idToken,
+        expirationTime
+      )
+    );
   }
 }
